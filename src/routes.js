d7d3848e7cc288f856635f68d89c47a5d46f53c8
maxwellsgomes <maxwellsgomes@hotmail.com>
2019-11-07 22:42:48 -0300

    Avatar Upload - route

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import StudentsController from './app/controllers/StudentsController';
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', authMiddleware, UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/students', authMiddleware, StudentsController.store);
+routes.put('/students', authMiddleware, StudentsController.update);
+routes.put('/users', authMiddleware, UserController.update);
+
+export default routes;
